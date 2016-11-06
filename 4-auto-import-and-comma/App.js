eb3f2ddd6803c026b148032b117820651e4ada9c
AveVlad <dev@vld.me>
2016-11-06 15:58:31 +0300

    4-auto-import-and-comma

---
+++ 4-auto-import-and-comma/App.js
@@ -0,0 +1,15 @@
+import {
+    MyFooFunction3,
+    MyKKKKKKFunction4,
+    MyTestFunction2,
+    FunctionWithVeryLongNaaaaaaaaaaaaaaame,
+    MyTestFunction8,
+} from './foo';
+
+
+console.log('-', MyFooFunction3());
+console.log('-', MyKKKKKKFunction4());
+console.log('-', MyTestFunction2());
+console.log('-', FunctionWithVeryLongNaaaaaaaaaaaaaaame());
+console.log('-', MyTestFunction8());
+console.log('-', Fffffffff());
---
+++ 4-auto-import-and-comma/foo.js
@@ -0,0 +1,6 @@
+export const MyTestFunction2 = () => null;
+export const MyFooFunction3 = () => null;
+export const MyKKKKKKFunction4 = () => null;
+export const FunctionWithVeryLongNaaaaaaaaaaaaaaame = () => null;
+export const MyTestFunction8 = () => null;
+export const Fffffffff = () => null;
